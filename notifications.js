8d7e629a757c400d83cbdd149cddad7ea89b8fdb
DCV <dcvslab@users.noreply.github.com>
2015-06-18 14:44:06 -0400

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -1,14 +1,24 @@
+//THIS IS A FIREFOX EXTENSION, THE CHROME EXTENSION IS IN THE MAKING//
+//COPYRIGHT DCVSLAB 2015, DO NOT EDIT WITHOUT GIVING ME CREDIT//
+
+//variables//
 var media = API.getMedia();
 var currentdj = API.getDJ().username;
+var userid = API.getUser().id;
+var currentpos = API.getWaitListPosition(userid) + 1
+
+//function running//
 window.onload = dcvStart();
 
 API.on(API.ADVANCE, function(data) {
         var media = API.getMedia();
         var currentdj = API.getDJ().username;
+        var currentpos = API.getWaitListPosition(userid) + 1
         setTimeout(function(){ dcvSong(); }, 3000);
+        setTimeout(function(){ dcvPos(); }, 6000);
 });
  
-
+//permission gainer//
 document.addEventListener('DOMContentLoaded', function () {
   if (Notification.permission !== "granted")
     Notification.requestPermission();
@@ -31,14 +41,13 @@ function dcvStart() {
 
    notification.onclick = function(){
     window.open("http://dcvslab.github.io/changelog");
-    window.focus();
-    this.cancel();
+    alert('Changelog has opened in a new tab')
                 }
         };
 }
-//song advance notifiaction//
 
-function dcvSong() {
+//song advance notifiaction//
+function dcvPos() {
   if (!Notification) {
     alert('Desktop notifications are not availible. Are you using the right script?'); 
     return;
@@ -47,33 +56,44 @@ function dcvSong() {
   if (Notification.permission !== "granted")
     Notification.requestPermission();
   else {
-        var media = API.getMedia();
         var currentdj = API.getDJ().username;
-        setTimeout(function(){ dcvClose() }, 6000);
     var notification = new Notification(currentdj + " is playing:", {
       icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-      body: (media.author+ "-" +media.title),
+      body: (media.author+ " - " +media.title),
     });
 
    notification.onclick = function(){
     window.focus();
-    this.cancel();
-    document.dispatchEvent(event);
 };
 
   }
-//the following is needed for chrome, i could use their notification api, but that blocks the chat//
 
-//close//
-function dcvClose() {
-       document.dispatchEvent(event);
 }
-//close event listener//
-document.addEventListener("close", function(e) {
-  console.log(e.detail); 
-});
 
-//close event create//
-var event = new CustomEvent("close", { "detail": "close event (needed for chrome)" });
+//you are posistion 1//
+if (currentpos = 0) {
+function dcvPos() {
+  if (!Notification) {
+    alert('Desktop notifications are not availible. Are you using the right script?'); 
+    return;
+  }
 
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+        var userid = API.getUser().id
+    var notification = new Notification("Booth Alert", {
+      icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+      body: ("You are up next!"),
+    });
+
+   notification.onclick = function(){
+    window.focus();
+};
+
+  }
+
+}
+} else {
+       console.log(currentpos) 
 }
