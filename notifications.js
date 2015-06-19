23011562d2582840a7a55162896aa4e6a4326753
DCV <dcvslab@users.noreply.github.com>
2015-06-19 13:41:44 -0400

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -61,8 +61,7 @@ function dcvTimeleft() {
          };
     var notification = new Notification(media.author + " - " + media.title), {
       icon: 'http://i.imgur.com/joWEdip.png',
-      body: (woots + wootgrammar + " | " + mehs + " " + mehgrammar " | " grabs + " " + grabgrammar),
-    };
+      body: (woots + " " + wootgrammar + " | " + mehs + " " + mehgrammar " | " grabs + " " + grabgrammar),)};
 
    notification.onclick = function(){
     window.focus();
