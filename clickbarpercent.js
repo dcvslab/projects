952cd0275089180e5db1b81c3dd6ee7bd9c370af
DCV <dcvslab@users.noreply.github.com>
2015-07-07 18:07:05 -0400

    Create clickbarpercent.js

---
+++ clickbarpercent.js
@@ -0,0 +1,8 @@
+//ADDON FOR RCS | http://rcs.radiant.dj //
+document.querySelector('div.bar').id = 'bar';
+var bar = document.getElementById("bar")
+
+function alertPercent {
+
+
+bar.addEventListener("click", alertPercent)
