589cc56c8de1cb82a29724220dbde95ca4dff56a
DCV <dcvslab@users.noreply.github.com>
2015-07-06 20:37:28 -0400

    Update clickwhois.js

--- clickwhois.js
+++ clickwhois.js
@@ -2,7 +2,7 @@ var listwoot = document.getElementById("woot-rs-list")
 var childrenwoot = listwoot.children;
 for (var i = 0; i < childrenwoot.length; i++) {
   var wootclick = childrenwoot[i];
-  wootlist.addEventListener("click", function(){
+  wootclick.addEventListener("click", function(){
     API.sendChat("/whois " + this.id);
 });
 }
