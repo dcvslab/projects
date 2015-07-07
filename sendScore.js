a09ea4b7e6613d0d80db1e463b55567cfde7cdd2
DCV <dcvslab@users.noreply.github.com>
2015-07-07 12:45:40 -0400

    Update sendScore.js

--- sendScore.js
+++ sendScore.js
@@ -1,12 +1,12 @@
 //define your variables//
-var woots = API.getScore().positive
-var mehs = API.getScore().negative
-var grabs = API.getScore().grabs
-var timeleft = API.getTimeRemaining()
+var woots = API.getScore().positive;
+var mehs = API.getScore().negative;
+var grabs = API.getScore().grabs;
+var timeleft = API.getTimeRemaining();
 
 //make a function to get the time//
 function getTime() {
-var timeleft = API.getTimeRemaining()
+var timeleft = API.getTimeRemaining();
 }
 
 //check the time every second//
@@ -17,10 +17,10 @@ function sendScore() {
 //tell it to do this when it reaches that second amount//
 if (timeleft == 5) {
 //redefine variables to get the current score//
-var woots = API.getScore().positive
-var mehs = API.getScore().negative
-var grabs = API.getScore().grabs
-console.log(woots + " woots | "mehs + " mehs | "grabs + " grabs")
+var woots = API.getScore().positive;
+var mehs = API.getScore().negative;
+var grabs = API.getScore().grabs;
+console.log(woots + " woots | "mehs + " mehs | "grabs + " grabs");
 //remember, you need to put spaces because the variables don't have them :P//
 
 //run the function every second as well, so it can test if timeleft == 5
