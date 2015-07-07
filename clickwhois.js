be66c3b841970d74aa11f9783a6957b28855acf5
DCV <dcvslab@users.noreply.github.com>
2015-07-06 21:55:21 -0400

    Update clickwhois.js

--- clickwhois.js
+++ clickwhois.js
@@ -1,4 +1,33 @@
 //by http://plug.dj/@/dcv with help from chip //
+var listwoot = document.getElementById("woot-rs-list")
+var childrenwoot = listwoot.children;
+var listgrab = document.getElementById("grab-rs-list")
+var childrengrab = listgrab.children;
+var listmeh = document.getElementById("meh-rs-list")
+var childrenmeh = listmeh.children;
+
+//woot list//
+for (var i = 0; i < childrenwoot.length; i++) {
+  var wootclick = childrenwoot[i];
+  wootclick.addEventListener("click", function(){
+    API.sendChat("/whois " + this.id);
+});
+}
+//grab list//
+for (var i = 0; i < childrengrab.length; i++) {
+  var wootgrab = childrengrab[i];
+  wootgrab.addEventListener("click", function(){
+    API.sendChat("/whois " + this.id);
+});
+}
+//meh list//
+for (var i = 0; i < childrenmeh.length; i++) {
+  var wootmeh = childrenmeh[i];
+  wootmeh.addEventListener("click", function(){
+    API.sendChat("/whois " + this.id);
+});
+}
+
 function hooDini() {
 var listwoot = document.getElementById("woot-rs-list")
 var childrenwoot = listwoot.children;
@@ -28,7 +57,3 @@ for (var i = 0; i < childrenmeh.length; i++) {
     API.sendChat("/whois " + this.id);
 });
 }
-API.on(API.ADVANCE, function(data) {
-  setTimeout(function(){ hooDini(); }, 10000);
-});
-window.onload = hooDini()
