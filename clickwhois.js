9a7bb79fab249143d29408b0ec23ed1977281282
DCV <dcvslab@users.noreply.github.com>
2015-07-06 21:07:56 -0400

    Update clickwhois.js

--- clickwhois.js
+++ clickwhois.js
@@ -27,35 +27,3 @@ for (var i = 0; i < childrenmeh.length; i++) {
     API.sendChat("/whois " + this.id);
 });
 }
-
-window.setInterval("varUpdate()", 1000);
-function varUpdate() {
-var listwoot = document.getElementById("woot-rs-list")
-var childrenwoot = listwoot.children;
-var listgrab = document.getElementById("grab-rs-list")
-var childrengrab = listgrab.children;
-var listmeh = document.getElementById("meh-rs-list")
-var childrenmeh = listmeh.children;
-
-//woot list//
-for (var i = 0; i < childrenwoot.length; i++) {
-  var wootclick = childrenwoot[i];
-  wootclick.addEventListener("click", function(){
-    API.sendChat("/whois " + this.id);
-});
-}
-//grab list//
-for (var i = 0; i < childrengrab.length; i++) {
-  var wootgrab = childrengrab[i];
-  wootgrab.addEventListener("click", function(){
-    API.sendChat("/whois " + this.id);
-});
-}
-//meh list//
-for (var i = 0; i < childrenmeh.length; i++) {
-  var wootmeh = childrenmeh[i];
-  wootmeh.addEventListener("click", function(){
-    API.sendChat("/whois " + this.id);
-});
-}
-}
