e9c178c89c634ba219b0470c5a8f570c2ea8981a
DCV <dcvslab@users.noreply.github.com>
2015-07-07 18:56:00 -0400

    Update clickbarpercent.js

--- clickbarpercent.js
+++ clickbarpercent.js
@@ -1,8 +1,13 @@
-//ADDON FOR RCS | http://rcs.radiant.dj //
+//ADDON FOR PLUGDJ | HTTP://PLUG.DJ //
 document.querySelector('div.bar').id = 'bar';
 var bar = document.getElementById("bar")
+document.querySelector('div.progress').id = 'progress';
+var progress = document.getElementById("progress");
+var percent = progress.style.width;
+var value = document.getElementsByClassName("value")[4];
+var inner = value.innerHTML;
 
-function alertPercent {
-
-
-bar.addEventListener("click", alertPercent)
+function cP {
+  inner = percent
+}
+ setInterval(function(){ alert("cP"); }, 10000);
