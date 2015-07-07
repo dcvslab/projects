24215fa6fc8e4cefe6f102e053250cf54d4e517d
DCV <dcvslab@users.noreply.github.com>
2015-07-07 13:53:25 -0400

    Done!
    I made it harder than I should have, fixed that and it works fine :D

--- sendScore.js
+++ sendScore.js
@@ -8,8 +8,7 @@ function getScore() {
 woots = API.getScore().positive;
 mehs = API.getScore().negative;
 grabs = API.getScore().grabs;
-console.log("SCORE_LOGGED")
-
+}
 //this updates the score every 10 seconds
 setInterval(function(){ getScore(); }, 10000);
 
@@ -19,5 +18,5 @@ API.sendChat("Woots: " + woots + " | Mehs: " + mehs + " | Grabs: " + grabs);
 }
 //this sends the score on the song advance for the previous song
 API.on(API.ADVANCE, function(data) {
-        setTimeout(function(){ sendScore(); }, 1000);
+        sendScore();
 });
