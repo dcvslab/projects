cfa86c42b50e8287bc3b59bda40b6661a1d75728
DCV <dcvslab@users.noreply.github.com>
2015-07-06 20:41:37 -0400

    Update clickwhois.js

--- clickwhois.js
+++ clickwhois.js
@@ -1,8 +1,29 @@
+//by http://plug.dj/@/dcv with help from chip //
+
+//woot list//
 var listwoot = document.getElementById("woot-rs-list")
 var childrenwoot = listwoot.children;
 for (var i = 0; i < childrenwoot.length; i++) {
   var wootclick = childrenwoot[i];
   wootclick.addEventListener("click", function(){
+    API.sendChat("/whois " + this.id);
+});
+}
+//grab list//
+var listgrab = document.getElementById("grab-rs-list")
+var childrengrab = listgrab.children;
+for (var i = 0; i < childrengrab.length; i++) {
+  var wootgrab = childrengrab[i];
+  wootgrab.addEventListener("click", function(){
+    API.sendChat("/whois " + this.id);
+});
+}
+//meh list//
+var listmeh = document.getElementById("meh-rs-list")
+var childrenmeh = listmeh.children;
+for (var i = 0; i < childrenmeh.length; i++) {
+  var wootmeh = childrenmeh[i];
+  wootmeh.addEventListener("click", function(){
     API.sendChat("/whois " + this.id);
 });
 }
