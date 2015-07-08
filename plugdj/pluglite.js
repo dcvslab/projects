e5677703aada72fa20e85ef30bc75b85b8229dbd
DCV <dcvslab@users.noreply.github.com>
2015-07-08 00:39:45 -0400

    Update and rename removechat.js to pluglite.js

---
+++ plugdj/pluglite.js
@@ -0,0 +1,24 @@
+//this is something kinda pointless, but :P
+
+//remove chat/friends buttons//
+document.getElementById("chat-button").style.display = "none";
+document.getElementById("friends-button").style.display = "none";
+
+//define buttons//
+var ubtn = document.getElementById("users-button");
+var wlbtn = document.getElementById("waitlist-button");
+//define background1
+var bg1 = document.getElementsByClassName("app-right")[0]
+
+//change ubtn properties//
+ubtn.style.left = "0px";
+ubtn.style.width = "50%";
+
+//chnge wlbtn properties//
+wlbtn.style.left = "50%";
+wlbtn.style.width = "50%";
+//change bg1 properties//
+bg1.style.backgroundColor = "rgba(10,10,10,.2)"
+
+//click on another option//
+ubtn.click();
