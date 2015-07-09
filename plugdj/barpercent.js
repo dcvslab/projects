6c87e17ec6d164f7db040c7f7e02ffe833574d11
DCV <dcvslab@users.noreply.github.com>
2015-07-09 18:58:26 -0400

    Update barpercent.js

--- plugdj/barpercent.js
+++ plugdj/barpercent.js
@@ -1,6 +1,10 @@
 // ADDON FOR PLUGDJ | HTTP://PLUG.DJ | BY DCV | HTTP://PLUG.DJ/@/DCV | HTTP://GITHUB.COM/DCVSLAB //
-document.querySelector('div.progress').id = 'progress';
-var progress = document.getElementById("progress");
+var progress = document.getElementsByClassName("progress")[0]; //change the xp bar to %
+progress.id = "progress";
 var percent = progress.style.width;
-function cP() { document.getElementsByClassName("value")[4].innerHTML = percent; percent = progress.style.width;}
+var xpvalue = document.getElementsByClassName("value")[4]
+function cP() {
+ percent = progress.style.width;
+ xpvalue.innerHTML = percent; }
 setInterval(function(){ cP(); }, 10000);
+cP()
