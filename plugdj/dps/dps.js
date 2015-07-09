002be359474990847123921a1e433bd84653b395
DCV <dcvslab@users.noreply.github.com>
2015-07-09 18:58:03 -0400

    Update dps.js

--- plugdj/dps/dps.js
+++ plugdj/dps/dps.js
@@ -64,10 +64,14 @@ dcvbtn.addEventListener("click", function(){ //here comes the actual script stuf
 });
 API.on(API.ADVANCE, function(data) { document.getElementById("woot").click();}); //autowoot//
 API.on(API.ADVANCE, function(data) { API.djJoin();}); //autojoin//
-document.querySelector('div.progress').id = 'progress'; //change the xp bar to %
-var progress = document.getElementById("progress");
+var progress = document.getElementsByClassName("progress")[0]; //change the xp bar to %
+progress.id = "progress";
 var percent = progress.style.width;
-function cP() { document.getElementsByClassName("value")[4].innerHTML = percent; percent = progress.style.width;}
+var xpvalue = document.getElementsByClassName("value")[4]
+function cP() {
+ percent = progress.style.width;
+ xpvalue.innerHTML = percent;
+}
 setInterval(function(){ cP(); }, 10000);
 cP()
 
