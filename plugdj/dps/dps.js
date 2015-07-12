39af83423343a72d5815ea965bc9a1e365cb28cd
DCV <dcvslab@users.noreply.github.com>
2015-07-12 17:49:39 -0400

    Update dps.js

--- plugdj/dps/dps.js
+++ plugdj/dps/dps.js
@@ -12,58 +12,75 @@ var sebtn = document.getElementsByClassName("settings button")[0];
 document.getElementById("facebook-menu").remove(); //remove fb
 document.getElementById("twitter-menu").remove(); //remove twitter
 document.getElementById("footer-user").style.width = "414px";
-ibtn.style.width = "16.6666666%";//change the width of all the buttons
-bbtn.style.width = "16.6666666%";
-stbtn.style.width = "16.6666666%";
-pbtn.style.width = "16.6666666%";
-sebtn.style.width = "16.6666666%";
-ibtn.style.left = "16.6666666%"; //move all the buttons over
-bbtn.style.left = "16.6666666%";
-stbtn.style.left = "16.6666666%";
-pbtn.style.left = "16.6666666%";
-sebtn.style.left = "16.6666666%";
+ibtn.style.width = bbtn.style.width = stbtn.style.width = pbtn.style.width = sebtn.style.width = ibtn.style.left = bbtn.style.left = stbtn.style.left = pbtn.style.left = sebtn.style.left = "16.6666666%";
 var backbtn = document.getElementsByClassName("back")[0] //fix the back button
 backbtn.style.width = "83.333333%"
 backbtn.style.left = "16.666666%"
-var dcvbtn = document.createElement("div"); //create dcv button
-dcvbtn.id = "dcv-button";
-dcvbtn.className = "dcv button";
-dcvbtn.style.right = "83.3333333%"; //a bit of styling
-dcvbtn.style.width = "16.66666666%";
-dcvbtn.style.zIndex ="1000";
-document.body.appendChild(dcvbtn); //put it in the document
-var dcvlogo = document.createElement("IMG"); //create dcv logo
-    dcvlogo.setAttribute("src", "http://i.imgur.com/1CuQDdE.png");
-    dcvlogo.setAttribute("width", "69");
-    dcvlogo.setAttribute("height", "53");
-    dcvlogo.setAttribute("alt", "dcvlogo");
-document.getElementById("dcv-button").appendChild(dcvlogo);
-var dcvlogox = document.createElement("IMG"); //create dcv logo
-    dcvlogox.setAttribute("src", "http://i.imgur.com/m8QFUVD.png");
-    dcvlogox.setAttribute("width", "69");
-    dcvlogox.setAttribute("height", "53");
-    dcvlogox.setAttribute("alt", "dcvlogox");
-    document.getElementById("dcv-button").appendChild(dcvlogox);
-document.getElementsByClassName("buttons")[0].appendChild(dcvbtn);
+var dpsbtn = document.createElement("div"); //create dps button
+dpsbtn.id = "dps-button";
+dpsbtn.className = "dps button";
+dpsbtn.style.right = "83.3333333%"; //a bit of styling
+dpsbtn.style.width = "16.66666666%";
+dpsbtn.style.zIndex ="1000";
+document.body.appendChild(dpsbtn); //put it in the document
+var dpslogo = document.createElement("IMG"); //create dps logo
+    dpslogo.setAttribute("src", "http://i.imgur.com/1CuQDdE.png");
+    dpslogo.setAttribute("width", "69");
+    dpslogo.setAttribute("height", "53");
+    dpslogo.setAttribute("alt", "dpslogo");
+document.getElementById("dps-button").appendChild(dpslogo);
+var dpslogox = document.createElement("IMG"); //create dps logo
+    dpslogox.setAttribute("src", "http://i.imgur.com/m8QFUVD.png");
+    dpslogox.setAttribute("width", "69");
+    dpslogox.setAttribute("height", "53");
+    dpslogox.setAttribute("alt", "dpslogox");
+    document.getElementById("dps-button").appendChild(dpslogox);
+document.getElementsByClassName("buttons")[0].appendChild(dpsbtn);
 var plbtn = document.getElementsByClassName("bar-button")[1] //to hide button when this is clicked
-function hidedcvBtn() { dcvbtn.style.display = "none" }
-plbtn.addEventListener("click", hidedcvBtn)
+function hidedpsBtn() { dpsbtn.style.display = "none" }
+plbtn.addEventListener("click", hidedpsBtn)
 var plbtnx = document.getElementById("playlist-button")
-function showdcvBtn() { dcvbtn.style.display = "block" }
-function showDcvbtn() { setTimeout(function(){ showdcvBtn(); }, 0200); }
-plbtnx.addEventListener("click", showDcvbtn)
-var dcvmenu = document.createElement("div"); //create the onclick menu
-dcvmenu.style.backgroundColor = "#282C35"; //styling
-dcvmenu.style.right = "345px";
-dcvmenu.style.display = "none";
-dcvmenu.id = "dcvmenu";
-dcvmenu.style.height = "inherit";
-dcvmenu.style.width = "345px";
-dcvmenu.style.zIndex = "10000";
-dcvmenu.style.position = "absolute";
-dcvmenu.style.borderBottom = "1px solid #0A0A0A"
+function showdpsBtn() { dpsbtn.style.display = "block" }
+function showdpsbtn() { setTimeout(function(){ showdpsBtn(); }, 0200); }
+plbtnx.addEventListener("click", showdpsbtn)
+var dpsmenu = document.createElement("div"); //create dps menu
+dpsmenu.style.backgroundColor = "#282C35"; //styling
+dpsmenu.style.right = "345px";
+dpsmenu.style.display = "none";
+dpsmenu.id = "dpsmenu";
+dpsmenu.style.height = "inherit";
+dpsmenu.style.width = "345px";
+dpsmenu.style.zIndex = "10000";
+dpsmenu.style.position = "absolute";
+dpsmenu.style.borderBottom = "1px solid #0A0A0A";
+var dpsmwoot = document.createElement("div"); //DPS MENU OPTIONS | woot option
+var dpsmwoottxt = document.createTextNode("Auto-Woot")
+dpsmwoot.className = "dpsmrow"
+dpsmwoot.appendChild(dpsmwoottxt);
+dpsmenu.appendChild(dpsmwoot);
+var dpsmautojoin = document.createElement("div"); //autojoin option
+var dpsmautojointxt = document.createTextNode("Auto-Join")
+dpsmautojoin.className = "dpsmrow"
+dpsmautojoin.appendChild(dpsmautojointxt);
+dpsmenu.appendChild(dpsmautojoin);
+var dpsmxppercent = document.createElement("div"); //xp to percent option
+var dpsmxppercenttxt = document.createTextNode("Show XP As A Percent")
+dpsmxppercent.className = "dpsmrow"
+dpsmxppercent.appendChild(dpsmxppercenttxt);
+dpsmenu.appendChild(dpsmxppercent);
+var dpsmfooterchange = document.createElement("div"); //changed footer option
+var dpsmfooterchangetxt = document.createTextNode("Auto-Join")
+dpsmfooterchange.className = "dpsmrow"
+dpsmfooterchange.appendChild(dpsmfooterchangetxt);
+dpsmenu.appendChild(dpsmchangedfooter);
+var dpsmclass = document.getElementsByClassName('dpsmrow'); //change row properties
+for(var i = 0; i < dpsmclass.length; i++) { 
+  dpsmclass[i].style.paddingTop = "10px"
+  dpsmclass[i].style.paddingBottom = "10px"
+}
+document.getElementsByClassName("app-right")[0].style.zIndex = "20" //make it so it's in front
 var plugmenu = document.getElementById("app-menu")
-document.getElementById("chat").appendChild(dcvmenu);
+document.getElementById("chat").appendChild(dpsmenu);
 var infobarclass = document.getElementsByClassName("info")[0]; //change the footer//
 infobarclass.id = "infobar";
 var infobar = document.getElementById("infobar");
@@ -79,21 +96,25 @@ btnsect.addEventListener("mouseleave", showInfo)
 var togglemenu = "no" //is needed
 function toggleMenu() { //toggle menu
 if (togglemenu == "no") {
-dcvmenu.style.display = "block"; //toggle some stuff
-dcvlogo.style.display = "none";
-dcvlogox.style.display = "block";
+dpsmenu.style.display = "block"; //toggle some stuff
+dpslogo.style.display = "none";
+dpslogox.style.display = "block";
 togglemenu = "yes"
 } else {
-dcvmenu.style.display = "none"; //toggle some stuff
-dcvlogo.style.display = "block";
-dcvlogox.style.display = "none";
+dpsmenu.style.display = "none"; //toggle some stuff
+dpslogo.style.display = "block";
+dpslogox.style.display = "none";
 togglemenu = "no"
 }}
-var wootopt = localStorage['wootopt'] || 'true'//load the options
-var autojoinopt = localStorage['autojoinopt'] || 'true'
-var xppercentopt = localStorage['xppercentopt'] || 'false'
-var infoshowingopt = localStorage['infoshowingopt'] || 'true'
-dcvbtn.addEventListener("click", toggleMenu) 
+var dpsopt = localStorage.getItem("dpsopt")
+if (! dpsopt){
+ 
+}
+var wootopt = localStorage.getItem("dpswootopt");//load the options
+var autojoinopt = localStorage.getItem("dpsautojoinopt")
+var xppercentopt = localStorage.getItem("dpsxppercentopt")
+var infoshowingopt = localStorage.getItem("dpswootopt")
+dpsbtn.addEventListener("click", toggleMenu) 
 API.on(API.ADVANCE, function(data) { document.getElementById("woot").click();}); //autowoot//
 API.on(API.ADVANCE, function(data) { API.djJoin();}); //autojoin//
 var progress = document.getElementsByClassName("progress")[0]; //change the xp bar to %
