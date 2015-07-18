814253ff4943020f974c748b08d6356685f30eab
DCV <dcvslab@users.noreply.github.com>
2015-07-17 20:51:24 -0400

    Update dps.js

--- plugdj/dps/dps.js
+++ plugdj/dps/dps.js
@@ -1,4 +1,4 @@
-//DCV'S PLUGDJ SCRIPT//
+//DCV'S PLUGDJ SCRIPT//ALPHA 02
 var userrole = API.getUser().role //get some important vars
 var userid = API.getUser().id
 var username = API.getUser().username
