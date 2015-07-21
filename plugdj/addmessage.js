7aa8ee26d5e8a0a29359aa63e2602d83b35530d3
DCV <dcvslab@users.noreply.github.com>
2015-07-20 22:57:38 -0400

    Update addmessage.js

--- plugdj/addmessage.js
+++ plugdj/addmessage.js
@@ -1,17 +1,30 @@
 //DCV'S ADD MESSAGE SCRIPT//
 var user = API.getUser()
 var damsn = 1
-var damsopt = 4
+var damsopt = 5
 var damseid = "DAMS-" + damsn
-function damS(badgebox, nameclass, nametext, messagetext) {
+function damsMessage(imgtype, badgebox, nameclass, nametext, messagetext) {
+  //IMGTYPE CAN BE 1 OF 2 THINGS: " class" will let you use plugdj icons and badges, while doing "mg src" and putting a link in the badgebox var will let you put an image
   if (! badgebox) { badgebox = "icon icon-plug-dj"; damsopt = damsopt - 1}
   if (! nameclass) { nameclass = "from staff"; damsopt = damsopt - 1 }
   if (! nametext) { nametext = "DAMS"; damsopt = damsopt - 1 }
   if (! messagetext) { messagetext = "DAMS MESSAGE TEXT"; damsopt = damsopt - 1 }
 user = API.getUser()
 var damsid = "DAMS-" + user.id + "-" + damsn + "-" + damsopt
-$("#chat-messages").append("<div data-cid='" + damsid + "' class='cm message' id=DAMS-" + damsn + "><div class='badge-box clickable'><i class='" + badgebox + "'></i></div><div class='msg cid-" + damsid + "'><div class='" + nameclass + "'><span class='un clickable'>" + nametext + "</span><span style='display: inline;' class='timestamp'></span></div><div class='text cid-" + damsid + "'>" + messagetext + "</div></div></div>");
+$("#chat-messages").append("<div data-cid='" + damsid + "' class='cm message' id=DAMS-" + damsn + "><div class='badge-box clickable'><i" + imgtype + "='" + badgebox + "'></i></div><div class='msg cid-" + damsid + "'><div class='" + nameclass + "'><span class='un clickable'>" + nametext + "</span></div><div class='text cid-" + damsid + "'>" + messagetext + "</div></div></div>");
 var one = 1
 damsnn = damsn + one
 damsn = damsnn
-dpsopt = 4 }
+dpsopt = 5 }
+//BORROWED FROM BETATESTER/IGOR ADDCHAT SCRIPT
+    var _scroll = $("#chat-messages")[0].scrollTop > $("#chat-messages")[0].scrollHeight - $("#chat-messages").height() - 28;
+
+    // If it should scroll, then scroll
+    if (_scroll) {
+        $("#chat-messages")[0].scrollTop = $("#chat-messages")[0].scrollHeight;
+    }
+
+    // If there's more than 512 messages, remove the last
+    if ($("#chat-messages").children().length > 512) {  $("#chat-messages").children().first().remove();  }
+
+// http://tgchan.org/kusaba/icons/src/awesome.png
