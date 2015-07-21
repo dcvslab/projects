d34f7508af0c0ef4e88b03c87c92e6a268e2f841
DCV <dcvslab@users.noreply.github.com>
2015-07-20 21:36:08 -0400

    Update addmessage.js

--- plugdj/addmessage.js
+++ plugdj/addmessage.js
@@ -1,23 +1,17 @@
 //DCV'S ADD MESSAGE SCRIPT//
-<div data-cid="dams-undefined" class="cm message"> 
-  <div class="badge-box clickable">
-    <i class="bdg bdg-dragon-g02 g"></i>
-  </div>
-  <div class="msg cid-dams-undefined"><div class="from you"> 
-    <span class="un clickable">DAMS</span>
-    <span style="display: inline;" class="timestamp"></span></div>
-      <div class="cid-dams-undefined">DAMS IS HERE</div>
-  </div>
-</div>
 var user = API.getUser()
 var damsn = 1
+var damsopt = 4
 var damseid = "DAMS-" + damsn
-function damS() {
+function damS(badgebox, nameclass, nametext, messagetext) {
+  if (! badgebox) { badgebox = "icon icon-plug-dj"; damsopt = damsopt - 1}
+  if (! nameclass) { nameclass = "from staff"; damsopt = damsopt - 1 }
+  if (! nametext) { nametext = "DAMS"; damsopt = damsopt - 1 }
+  if (! messagetext) { messagetext = "DAMS MESSAGE TEXT"; damsopt = damsopt - 1 }
 user = API.getUser()
-var damsid = "DAMS-" + user.id + "-" + damsn
-$("head").append("<div data-cid='" + damsid + "' class='cm message' id=DAMS-" + damsn + "><div class='badge-box clickable'><i class='" + badgebox + "'></i></div><div class=msg cid-'" + damsid + "'><div class='" + nameclass + "'>"
-);
+var damsid = "DAMS-" + user.id + "-" + damsn + "-" + damsopt
+$("#chat-messages").append("<div data-cid='" + damsid + "' class='cm message' id=DAMS-" + damsn + "><div class='badge-box clickable'><i class='" + badgebox + "'></i></div><div class='msg cid-" + damsid + "'><div class='" + nameclass + "'><span class='un clickable'>" + nametext + "</span><span style='display: inline;' class='timestamp'></span></div><div class='text cid-" + damsid + "'>" + messagetext + "</div></div></div>");
 var one = 1
 damsnn = damsn + one
 damsn = damsnn
-}
+dpsopt = 4 }
