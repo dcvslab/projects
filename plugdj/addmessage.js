d185c3a3fb0cb1369135047e3decb903a63f126a
DCV <dcvslab@users.noreply.github.com>
2015-07-21 22:03:16 -0400

    Update addmessage.js

--- plugdj/addmessage.js
+++ plugdj/addmessage.js
@@ -2,28 +2,21 @@
 var user = API.getUser()
 var damsn = 1
 var damseid = "DAMS-" + damsn
- var _scroll = $("#chat-messages")[0].scrollTop > $("#chat-messages")[0].scrollHeight - $("#chat-messages").height() - 28;
-function damsMessage(imgtype, badgebox, nameclass, nametext, messagetext) {
+var icon1 = 0
+var icon2 = 0
+var icon3 = 0
+var _scroll = $("#chat-messages")[0].scrollTop > $("#chat-messages")[0].scrollHeight - $("#chat-messages").height() - 28; //BORROWED WITH PERMISSION FROM BETATESTER/IGOR ADDCHAT SCRIPT
+function damsMessage(imgtype, badgebox, nameclass, nametext, nametextvar,  messagetext, messagetextvar) { 
   //IMGTYPE CAN BE 1 OF 2 THINGS: " class" | THE SPACE IS IMPORTANT | will let you use plugdj icons and badges, while doing "mg src" and putting a link in the badgebox var will let you put an image
-  if (! imgtype) { imgtype = " class";}
-  if (! badgebox) { badgebox = "icon icon-plug-dj";}
-  if (! nameclass) { nameclass = "staff";}
-  if (! nametext) { nametext = "DAMS";}
-  if (! messagetext) { messagetext = "DAMS MESSAGE TEXT";}
+  //BADGEBOX CAN BE DONE USING PLUGDJ ICONS( http://pastebin.com/QD9FAE4d has a list of all icons), PLUGDJ BADGES (https://rcs.radiant.dj/badges has almost all badges, just put bdg bdg- infront ) OR THE LINK TO YOUR IMAGE IF YOU ARE DOING "mg src"
+  //NAMECLASS can be done using a plug.dj class (from you, from(will do grey user), from staff, from ambassador, from admin) or your own class. To add has sub, do from has-sub "insert rest"
+    //IMPORTANT //when using pdj ones, YOU NEED THE FROM IN FRONT
+ //NAMETEXT is the text where the username goes, you can use vars, one is installed
+ //MESSAGETEXT is the text where the chat goes, you can use vars, one is installed
 user = API.getUser()
 var damsid = "DAMS-" + user.id + "-" + damsn
-$("#chat-messages").append("<div data-cid='" + damsid + "' class='cm message' id=DAMS-" + damsn + "><div class='badge-box clickable'><i" + imgtype + "='" + badgebox + "'></i></div><div class='msg cid-" + damsid + "'><div class=from '" + nameclass + "'><span class='un clickable'>" + nametext + "</span></div><div class='text cid-" + damsid + "'>" + messagetext + "</div></div></div>");
+$("#chat-messages").append("<div data-cid='" + damsid + "' class='cm message'><div class='badge-box clickable'><i" + imgtype + "='" + badgebox + "'></i></div><div class='msg cid-" + damsid + "'><div class='" + nameclass + "'><span class='un clickable'>" + nametext + nametextvar +  "</span></div><div class='text cid-" + damsid + "'>" + messagetext + messagetextvar + "</div></div></div>");
 var one = 1
-damsn = damsn + one}
-//BORROWED FROM BETATESTER/IGOR ADDCHAT SCRIPT
-
-
-    // If it should scroll, then scroll
-    if (_scroll) {
-        $("#chat-messages")[0].scrollTop = $("#chat-messages")[0].scrollHeight;
-    }
-
-    // If there's more than 512 messages, remove the last
-    if ($("#chat-messages").children().length > 512) {  $("#chat-messages").children().first().remove();  }
-
-// http://tgchan.org/kusaba/icons/src/awesome.png
+damsn = damsn + one }
+if (_scroll) { $("#chat-messages")[0].scrollTop = $("#chat-messages")[0].scrollHeight; } //BORROWED WITH PERMISSION FROM BETATESTER/IGOR ADDCHAT SCRIPT
+if ($("#chat-messages").children().length > 512) {  $("#chat-messages").children().first().remove();  } //BORROWED WITH PERMISSION FROM BETATESTER/IGOR ADDCHAT SCRIPT
