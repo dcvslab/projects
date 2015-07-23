7bf7516d5fc5414dc0dcc07f6830c4748423aa0a
DCV <dcvslab@users.noreply.github.com>
2015-07-22 22:34:22 -0400

    Update addmessage.js

--- plugdj/addmessage.js
+++ plugdj/addmessage.js
@@ -4,6 +4,7 @@ var damsn = 1
 var damseid = "DAMS-" + damsn
 var damsid = "DAMS-" + user.id + "-" + damsn
 var _scroll = $("#chat-messages")[0].scrollTop > $("#chat-messages")[0].scrollHeight - $("#chat-messages").height() - 28; //BORROWED WITH PERMISSION FROM BETATESTER/IGOR ADDCHAT SCRIPT
+console.log ("FUNCTION ORDER: imgtype, badgebox, nameclass, nametext, nametextvar,  messagetext, messagetextvar")
 function damsMessage(imgtype, badgebox, nameclass, nametext, nametextvar,  messagetext, messagetextvar) { 
   //IMGTYPE CAN BE 1 OF 2 THINGS: " class" | THE SPACE IS IMPORTANT | will let you use plugdj icons and badges, while doing "mg src" and putting a link in the badgebox var will let you put an image
   //BADGEBOX CAN BE DONE USING PLUGDJ ICONS( http://pastebin.com/QD9FAE4d has a list of all icons), PLUGDJ BADGES (https://rcs.radiant.dj/badges has almost all badges, just put bdg bdg- infront ) OR THE LINK TO YOUR IMAGE IF YOU ARE DOING "mg src"
@@ -12,11 +13,10 @@ function damsMessage(imgtype, badgebox, nameclass, nametext, nametextvar,  messa
  //NAMETEXT is the text where the username goes, you can use vars, one is installed 
  //MESSAGETEXT is the text where the chat goes, you can use vars, one is installed
     //IMPORTANT //when using vars, don't put quotes around them when inputting the function ex: "messagetext", messagetextvar
-$("#chat-messages").append("<div data-cid='" + damsid + "' class='cm message'><div class='badge-box clickable'><i" + imgtype + "='" + badgebox + "'></i></div><div class='msg cid-" + damsid + "'><div class='" + nameclass + "'><span class='un clickable'>" + nametext + nametextvar +  "</span></div><div class='text cid-" + damsid + "'>" + messagetext + messagetextvar + "</div></div></div>");
+$("#chat-messages").append("<div data-cid='" + damsid + "' class='cm message'><div class='badge-box'><i" + imgtype + "='" + badgebox + "'></i></div><div class='msg cid-" + damsid + "'><div class='" + nameclass + "'><span class='un'>" + nametext + nametextvar +  "</span></div><div class='text cid-" + damsid + "'>" + messagetext + messagetextvar + "</div></div></div>");
 var one = 1
 damsn = damsn + one
 damseid = "DAMS-" + damsn
-damsid = "DAMS-" + user.id + "-" + damsn 
-console.log ("FUNCTION ORDER: imgtype, badgebox, nameclass, nametext, nametextvar,  messagetext, messagetextvar")}
+damsid = "DAMS-" + user.id + "-" + damsn }
 if (_scroll) { $("#chat-messages")[0].scrollTop = $("#chat-messages")[0].scrollHeight; } //BORROWED WITH PERMISSION FROM BETATESTER/IGOR ADDCHAT SCRIPT
 if ($("#chat-messages").children().length > 512) {  $("#chat-messages").children().first().remove();  } //BORROWED WITH PERMISSION FROM BETATESTER/IGOR ADDCHAT SCRIPT
