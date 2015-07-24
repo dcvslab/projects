9e58f872638e7a997cee1105c536b21fb1d35e19
DCV <dcvslab@users.noreply.github.com>
2015-07-23 21:42:48 -0400

    1.3

--- plugdj/addmessage.js
+++ plugdj/addmessage.js
@@ -1,11 +1,12 @@
-//DCV'S ADD MESSAGE SCRIPT// COMING SOON: TIMESTAMPS? V1.2
+//DCV'S ADD MESSAGE SCRIPT// COMING SOON: TIMESTAMPS? V1.3
 var user = API.getUser()
 var damsn = 1
 var damseid = "DAMS-" + damsn
 var damsid = "DAMS-" + user.id + "-" + damsn
 var time = "DAMS"
+var icon1display = "none"; icon2display = "none"; icon3display = "none";
 var _scroll = $("#chat-messages")[0].scrollTop > $("#chat-messages")[0].scrollHeight - $("#chat-messages").height() - 28; //BORROWED WITH PERMISSION FROM BETATESTER/IGOR ADDCHAT SCRIPT
-console.log ("FUNCTION ORDER: imgtype, badgebox, nameclass, nametext, nametextvar,messagetext, messagetextvar, icon1type, icon1data, icon2type, icon2data, icon3type, icon3data")
+console.log ("FUNCTION ORDER: imgtype, badgebox, nameclass, nametext, nametextvar, messagetext, messagetextvar, icon1type, icon1data, icon2type, icon2data, icon3type, icon3data")
 function damsMessage(imgtype, badgebox, nameclass, nametext, nametextvar, messagetext, messagetextvar, icon1type, icon1data, icon2type, icon2data, icon3type, icon3data) { 
   //IMGTYPE CAN BE 1 OF 2 THINGS: " class" | THE SPACE IS IMPORTANT | will let you use plugdj icons and badges, while doing "mg src" and putting a link in the badgebox var will let you put an image
   if (! imgtype) { imgtype="" }
@@ -25,15 +26,15 @@ function damsMessage(imgtype, badgebox, nameclass, nametext, nametextvar, messag
     //IMPORTANT //when using vars, don't put quotes around them when inputting the function ex: "messagetext", messagetextvar
  //ICONS
   //ICON TYPE: EITHER " class" or "mg src", the first for using plugdj icons and the second for using your own images
-  if (! icon1type) { icon1type="" }
-  if (! icon2type) { icon2type="" }
-  if (! icon3type) { icon3type="" }
+  if (! icon1type) { icon1type=""; icon1display = "none"} else { icon1display = "block" } 
+  if (! icon2type) { icon2type=""; icon2display = "none" } else { icon2display = "block" } 
+  if (! icon3type) { icon3type=""; icon3display = "none" } else { icon3display = "block" } 
   //ICON DATA: where you put the icon/link you want for the image
-  if (! icon1data) { icon1data="" }
+  if (! icon1data) { icon1data=""}
   if (! icon2data) { icon2data="" }
   if (! icon3data) { icon3data="" }
     //IMPORTANT // if you don't want to use icons, you don't have to! that's why they are at the end so you don't even have to bother with them
-$("#chat-messages").append("<div data-cid='" + damsid + "' class='cm message'><div class='badge-box'><i" + imgtype + "='" + badgebox + "'></i></div><div class='msg cid-" + damsid + "'><div class='" + nameclass + "'><i" + icon1type + "='" + icon1data + "' style='margin-right: 4px; width: 16px; height: 16px; float: left;'></i><i" + icon2type + "='" + icon2data + "' style='margin-right: 4px; width: 16px; height: 16px; float: left;'></i><i" + icon3type + "='" + icon3data + "' style='margin-right: 4px; width: 16px; height: 16px; float: left;'></i><span class='un'>" + nametext + nametextvar +  "</span><span style='display: inline;' class='timestamp'>" + time + "</span></div><div class='text cid-" + damsid + "'>" + messagetext + messagetextvar + "</div></div></div>");
+$("#chat-messages").append("<div data-cid='" + damsid + "' class='cm message'><div class='badge-box'><i" + imgtype + "='" + badgebox + "'></i></div><div class='msg cid-" + damsid + "'><div class ='" + nameclass + "'><i" + icon1type + " ='" + icon1data + "' style='margin-right: 4px; width: 16px; height: 16px; float: left; display: " + icon1display + ";'></i><i" + icon2type + " ='" + icon2data + "' style='margin-right: 4px; width: 16px; height: 16px; float: left; display: " + icon2display + ";'></i><i" + icon3type + " ='" + icon3data + "' style='margin-right: 4px; width: 16px; height: 16px; float: left; display: " + icon3display + ";'></i><span class='un'>" + nametext + nametextvar +  "</span><span style='display: inline;' class='timestamp'>" + time + "</span></div><div class='text cid-" + damsid + "'>" + messagetext + messagetextvar + "</div></div></div>");
 var imgtype="";var badgebox="";var nameclass="";var nametext="";var nametextvar;var messagetext="";var messagetextvar="";var icon1type="";var icon1data="";var icon2type=""; icon2data="";var icon3type="";var icon3data="";
 damsn = damsn + 1
 damseid = "DAMS-" + damsn
