4ce0a465d84ee8bc18bac940e3763d4156de289d
DCV <dcvslab@users.noreply.github.com>
2015-07-23 23:35:29 -0400

    Update addmessage.js

--- plugdj/addmessage.js
+++ plugdj/addmessage.js
@@ -1,9 +1,11 @@
-//DCV'S ADD MESSAGE SCRIPT// COMING SOON: TIMESTAMPS? V1.3
+//DCV'S ADD MESSAGE SCRIPT// COMING SOON: TIMESTAMPS? V1.4.1
 var user = API.getUser()
 var damsn = 1
 var damseid = "DAMS-" + damsn
 var damsid = "DAMS-" + user.id + "-" + damsn
-var time = "DAMS"
+var hour = new Date().toTimeString().split(" ")[0].split(":")[0]; var minute = new Date().toTimeString().split(" ")[0].split(":")[1]; 
+if (hour > 11) { ampm = "pm"; } else { ampm = "am" }
+var time = hour  + ":" + minute + ampm + " | DAMS";
 var icon1display = "none"; icon2display = "none"; icon3display = "none";
 var _scroll = $("#chat-messages")[0].scrollTop > $("#chat-messages")[0].scrollHeight - $("#chat-messages").height() - 28; //BORROWED WITH PERMISSION FROM BETATESTER/IGOR ADDCHAT SCRIPT
 console.log ("FUNCTION ORDER: imgtype, badgebox, nameclass, nametext, nametextvar, messagetext, messagetextvar, icon1type, icon1data, icon2type, icon2data, icon3type, icon3data")
@@ -12,7 +14,7 @@ function damsMessage(imgtype, badgebox, nameclass, nametext, nametextvar, messag
   if (! imgtype) { imgtype="" }
   //BADGEBOX CAN BE DONE USING PLUGDJ ICONS( http://pastebin.com/QD9FAE4d has a list of all icons), PLUGDJ BADGES (https://rcs.radiant.dj/badges has almost all badges, just put bdg bdg- infront ) OR THE LINK TO YOUR IMAGE IF YOU ARE DOING "mg src"
   if (! badgebox) { badgebox="" }
-  //NAMECLASS can be done using a plug.dj class (from you, from(will do grey user), from staff, from ambassador, from admin) or your own class. To add has sub, do from has-sub "insert rest"
+  //NAMECLASS can be done using a plug.dj class (from you, from(will do grey user), from staff, from ambassador, from admin) or your own class. To add has sub, do from has-sub subscriber
     //IMPORTANT //when using pdj ones, YOU NEED THE FROM IN FRONT
   if (! nameclass) { nameclass="" }
  //NAMETEXT is the text where the username goes, you can use vars, one is installed 
@@ -34,6 +36,10 @@ function damsMessage(imgtype, badgebox, nameclass, nametext, nametextvar, messag
   if (! icon2data) { icon2data="" }
   if (! icon3data) { icon3data="" }
     //IMPORTANT // if you don't want to use icons, you don't have to! that's why they are at the end so you don't even have to bother with them
+
+var hour = new Date().toTimeString().split(" ")[0].split(":")[0]; var minute = new Date().toTimeString().split(" ")[0].split(":")[1]; 
+if (hour > 12) { ampm = "pm"; hourb = hour + 12; hour = hourb - 24  } else { ampm = "am" }
+var time = hour  + ":" + minute + ampm + " | DAMS";
 $("#chat-messages").append("<div data-cid='" + damsid + "' class='cm message'><div class='badge-box'><i" + imgtype + "='" + badgebox + "'></i></div><div class='msg cid-" + damsid + "'><div class ='" + nameclass + "'><i" + icon1type + " ='" + icon1data + "' style='margin-right: 4px; width: 16px; height: 16px; float: left; display: " + icon1display + ";'></i><i" + icon2type + " ='" + icon2data + "' style='margin-right: 4px; width: 16px; height: 16px; float: left; display: " + icon2display + ";'></i><i" + icon3type + " ='" + icon3data + "' style='margin-right: 4px; width: 16px; height: 16px; float: left; display: " + icon3display + ";'></i><span class='un'>" + nametext + nametextvar +  "</span><span style='display: inline;' class='timestamp'>" + time + "</span></div><div class='text cid-" + damsid + "'>" + messagetext + messagetextvar + "</div></div></div>");
 var imgtype="";var badgebox="";var nameclass="";var nametext="";var nametextvar;var messagetext="";var messagetextvar="";var icon1type="";var icon1data="";var icon2type=""; icon2data="";var icon3type="";var icon3data="";
 damsn = damsn + 1
@@ -41,3 +47,4 @@ damseid = "DAMS-" + damsn
 damsid = "DAMS-" + user.id + "-" + damsn }
 if (_scroll) { $("#chat-messages")[0].scrollTop = $("#chat-messages")[0].scrollHeight; } //BORROWED WITH PERMISSION FROM BETATESTER/IGOR ADDCHAT SCRIPT
 if ($("#chat-messages").children().length > 512) {  $("#chat-messages").children().first().remove();  } //BORROWED WITH PERMISSION FROM BETATESTER/IGOR ADDCHAT SCRIPT
+damsMessage(" class", "bdg bdg-dragon-g02 g", "from has-sub subscriber", "DCV | DAMS Creator", "", "DAMS has been activated!", "", " class", "icon icon-chat-subscriber")
