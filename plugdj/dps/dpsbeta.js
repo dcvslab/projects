3ad5b4d0e3023fba08829e2e200eab04b8492d81
DCV <dcvslab@users.noreply.github.com>
2015-07-29 20:49:23 -0400

    A5.4.4B

--- plugdj/dps/dpsbeta.js
+++ plugdj/dps/dpsbeta.js
@@ -2,7 +2,7 @@
 //DCV'S PLUGDJ SCRIPT//ALPHA 05.2 BETA// HTTP://PLUG.DJ/DCV // HTTP://DCVSLAB.GITHUB.IO // DCVSLAB.GITHUB.IO
 if (! on) {
 var on = "yes"
-var release = "ALPHA"; var vnum = "05"; var subvnum = "4"; var commitnum = "3"; var beta = "BETA"
+var release = "ALPHA"; var vnum = "05"; var subvnum = "4"; var commitnum = "4"; var beta = "BETA"
 var version = release + " " + vnum + "." + subvnum + "." + commitnum + " " + beta
 var user = API.getUser();
 var media = API.getMedia();
@@ -142,7 +142,6 @@ else {
 dpsAw();
 function dpsAj() { //autojoin//
   if (ajopt == "true") {
-    API.djJoin(); console.log("joined"); 
     dpsmcheckaj.style.visibility = "visible" }
   else {
     dpsmcheckaj.style.visibility = "hidden" }}
@@ -151,15 +150,18 @@ dpsAj();
 //song stats
 var ssuserid = API.getHistory()[0].user.id
 var ssuser = API.getUser(ssuserid)
-var ssucolor = "#FFF"
+var ssucolor;
 function dpsSs() { 
 if (ssopt == "true") {
   ssuserid = API.getHistory()[0].user.id
   ssuser = API.getUser(ssuserid)
-  if (user.id == ssuserid) { if (user.role == "0") { if (user.gRole == "0") { if (user.sub == "0") { ssucolor = "#FFDD6F"}}}} else {
-  if (user.gRole == "0") { if (user.role > 0) { ucolor = "#AC76FF" } else { if (user.sub == 1) { ucolor = "#C59840" } else { ucolor = "#777F92" } } } else { if (user.gRole == "3") { ucolor = "#89BE6C" } else { ucolor = "#42A5DC" } }
-  dpsMessaged("message", "from admin", "SONG STATS","<b><font color='" + ssucolor + "'>" + API.getHistory()[0].user.username + "</b></font> played <b>" + API.getHistory()[0].media.author + " - " + API.getHistory()[0].media.title + "</b>", "<b><font color='#90AD2F'>" + API.getHistory()[0].score.positive + " woots</font></b> | <b><font color='#C42E3B'>" + API.getHistory()[0].score.negative + " mehs</font></b> | <font color='#AA74FF'>" + API.getHistory()[0].score.grabs + " grabs</font></b> | <font color='#8493b0'><b>" + API.getHistory()[0].score.listeners + " users</b></font>" )
-}}}
+  ssucolor;
+  setTimeout(function(){ 
+  if (user.id == ssuserid) { if (ssuser.role == "0") { if (ssuser.gRole == "0") { if (ssuser.sub == "0") { ssucolor = "#FFDD6F"}}}} else {
+  if (user.gRole == "0") { if (user.role > 0) { ssucolor = "#AC76FF" } else { if (ssuser.sub == 1) { ssucolor = "#C59840" } else { ssucolor = "#777F92" } } } else { if (ssuser.gRole == "3") { ssucolor = "#89BE6C" } else { ssucolor = "#42A5DC" } }
+  dpsMessaged("message", "from admin", "SONG STATS","<b><font color='" + ssucolor + "'>" + API.getHistory()[1].user.username + "</b></font> played <b>" + API.getHistory()[1].media.author + " - " + API.getHistory()[1].media.title + "</b>", "<b><font color='#90AD2F'>" + API.getHistory()[1].score.positive + " woots</font></b> | <b><font color='#C42E3B'>" + API.getHistory()[1].score.negative + " mehs</font></b> | <font color='#AA74FF'>" + API.getHistory()[0].score.grabs + " grabs</font></b> | <font color='#8493b0'><b>" + API.getHistory()[1].score.listeners + " users</b></font>" )
+  }}, 1000); }}
+function dpsssCheck() {if (ssopt == "true") {dpsmcheckss.style.visibility = "visible" } else {dpsmcheckss.style.visibility = "hidden"}}
 //STYLING SETTINGS//
 var progress = document.getElementsByClassName("progress")[0]; //change the xp bar to %
 progress.id = "progress";
@@ -239,15 +241,17 @@ function menuClicked() { //to set up the menu
     dpsoptUpdate(); 
     setTimeout(function () { dpsAj() }, 0050); } 
   else { 
-    ajopt = "true"; 
+    ajopt = "true";
     dpsoptUpdate(); 
     setTimeout(function () { dpsAj() }, 0050); }} 
   dpsmss.onclick=function(){ if (ssopt == "true") { 
     ssopt = "false"; 
-    dpsoptUpdate(); } 
+    dpsmcheckss.style.visibility = "hidden"
+    dpsoptUpdate(); dpsssCheck() } 
   else { 
-    ssopt = "true"; 
-    dpsoptUpdate(); }}
+    ssopt = "true";
+    dpsmcheckss.style.visibility = "visible"
+    dpsoptUpdate(); dpsssCheck() }}
   dpsmxpp.onclick=function(){ if (xppopt == "true") { 
     xppopt = "false"; 
     dpsoptUpdate(); 
