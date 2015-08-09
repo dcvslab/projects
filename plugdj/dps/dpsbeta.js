f86f61d77b289052c13222ad09552c389d27ff4a
DCV <dcvslab@users.noreply.github.com>
2015-08-08 23:54:29 -0400

    A7.2.3.2B

--- plugdj/dps/dpsbeta.js
+++ plugdj/dps/dpsbeta.js
@@ -1,7 +1,7 @@
 //DCV'S PLUGDJ SCRIPT//ALPHA 06 RELEASE// HTTP://PLUG.DJ/DCV // HTTP://DCVSLAB.GITHUB.IO // DCVSLAB.GITHUB.IO
 if (! on) {
 var on = "on"
-var release = "ALPHA"; var vnum = "7"; var subvnum = "2"; var commitnum = "3.1"; var beta = "BETA"
+var release = "ALPHA"; var vnum = "7"; var subvnum = "2"; var commitnum = "3.2"; var beta = "BETA"
 var version = release + " " + vnum + "." + subvnum + "." + commitnum + " " + beta
 var user = API.getUser();
 var media = API.getMedia();
@@ -441,7 +441,7 @@ if (cmd.contains("/debug")) { //debug commands
   user = API.getUser(); if (user.id == creator.id) {
   if (cmd.split("/debug ")[1] == "reset") { localStorage.removeItem("dpsOpt"); API.chatLog("DPS has been reset! Refreshing..."); setTimeout(function(){ location.reload(); }, 1000); }
   if (cmd.split("/debug ")[1] == "refresh" || cmd.split("/debug ")[1] == "reload") { location.reload() }
-  if (cmd.split("/debug ")[1] == "timeon") { if (timeon > 3600) { API.chatLog("DPS has been running for " + cmd.split(" ")[0] + cmd.split(" ")[1] + cmd.split(" ")[2] + " hours.") } else { if (timeon > 60) { API.chatLog("DPS has been running for " + cmd.split(" ")[0] + cmd.split(" ")[1] + cmd.split(" ")[2] + " minutes." })
+  if (cmd.split("/debug ")[1] == "timeon") { API.chatLog("DPS has been running for " + timeon + " seconds.") }
   } else { dpsMessage("system", "from", "DPS ALERT", "Debug commands are currently only availible to <b class='dpssub'>" + creator.username + "</b>. Check back later!")}
 }}
 API.on(API.CHAT_COMMAND, chatcmd);
