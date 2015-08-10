4962cd58e1227808a53956df60e4d72bbcfbf87f
DCV <dcvslab@users.noreply.github.com>
2015-08-09 21:36:20 -0400

    Update mehusers.js

--- plugdj/mehusers.js
+++ plugdj/mehusers.js
@@ -1,17 +1,26 @@
 var dj = API.getDJ()
 var amehuser;
-localStorage.setItem("amu", "")
-function chatcommand(cmd) { 
-if (cmd.contains("/amu")) {
-if (cmd.split("/amu ")[1].contains(",") { }
-else {
-var mehuser = cmd.split("/amu ")[1]
-
+function chatcommand(command) {
+if (command.contains("/amul")) {
+  var getamul = localStorage.getItem("mehuser");
+  API.chatLog("Current List: " + getamul);
+} else {
+if (command.contains("/amu")) {
+if (command.split("/amu ")[1].contains(",")) { 
+mehuser = command.split("/amu ")[1].split(",")
+localStorage.setItem("mehuser", mehuser)
+API.chatLog("Users " + mehuser + " were added to your list!")
 }
+else {
+var mehuser = command.split("/amu ")[1];
+localStorage.setItem("mehuser", mehuser)
+API.chatLog("User " + mehuser + " was added to your list!")
+}}}}
 function amehUser() {
 dj = API.getDJ()
 var amuc = amehuser.children;
 for (var i = 0; i < amuc.length; i++) {
-  if (dj == amuc[i]) {
+  if (dj.contains(amuc[i])) {
     document.getElementById("meh").click() }}}
 API.on(API.ADVANCE, function(data) { amehUser(); })
+API.on(API.CHAT_COMMAND, chatcommand);
