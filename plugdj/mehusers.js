d1b678e3a0e7a251096c2178a5136492751ccc1b
DCV <dcvslab@users.noreply.github.com>
2015-08-09 21:15:37 -0400

    Create mehusers.js

---
+++ plugdj/mehusers.js
@@ -0,0 +1,17 @@
+var dj = API.getDJ()
+var amehuser;
+localStorage.setItem("amu", "")
+function chatcommand(cmd) { 
+if (cmd.contains("/amu")) {
+if (cmd.split("/amu ")[1].contains(",") { }
+else {
+var mehuser = cmd.split("/amu ")[1]
+
+}
+function amehUser() {
+dj = API.getDJ()
+var amuc = amehuser.children;
+for (var i = 0; i < amuc.length; i++) {
+  if (dj == amuc[i]) {
+    document.getElementById("meh").click() }}}
+API.on(API.ADVANCE, function(data) { amehUser(); })
