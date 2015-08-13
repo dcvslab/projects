63cb5b4d29e85e5496d151cde28511f1dce0d2c1
DCV <dcvslab@users.noreply.github.com>
2015-08-13 18:40:52 -0400

    Update cmdlog.js

--- plugdj/cmdlog.js
+++ plugdj/cmdlog.js
@@ -1,5 +1,10 @@
-<div class="item cmdlog clickable">
-        <i class="icon icon-chat-system"></i>
-        <span>Command Log</span>
-    </div>
-var cmdlogbtn = document.createElement("div")
+var cmdlogbtn = document.createElement("div"); cmdlogbtn.className = "item cmdlog clickable";
+var cmdlogbtni = document.createElement("i"); cmdlogbtni.className = "icon icon-chat-system";
+var cmdlogbtntxt = document.createElement("span"); cmdlogbtntxt.innerHTML = "Command Log";
+var list = document.getElementsByClassName("list")[2]
+$(cmdlogbtn).append(cmdlogbtntxt); $(cmdlogbtn).prepend(cmdlogbtni); $(list).prepend(cmdlogbtn);
+var cmdlogm = document.createElement("div"); cmdlogm.style.width = document.getElementById("dashboard").style.width; cmdlogm.style.height = "inherit"; cmdlogm.style.backgroundColor = "#111317"; cmdlogm.style.right = "345px"  
+var appright = document.getElementsByClassName("app-right")[0]
+$(appright).append(cmdlogm)
+function showClm() { cmdlogm.style.display = "block" }
+cmdlogbtn.addEventListener("click", showClm)
