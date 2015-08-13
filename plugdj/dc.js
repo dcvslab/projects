b370734f8cc05e86d1736cb6b8c3f742a4ece5f1
DCV <dcvslab@users.noreply.github.com>
2015-08-12 22:43:10 -0400

    Update dc.js

--- plugdj/dc.js
+++ plugdj/dc.js
@@ -15,9 +15,13 @@ function uJ(uj) {
   var wldcl = wldc.length;
   for (var i = 0; i < wldcl; i++) {
    if (wldc[i].split(".")[1] == uj.id) {
-    if ( JSON.parse(wldc[i].split(".")[3]) <= JSON.parse(wldc[i].split(".")[3]) + 3600) { 
+    var sthree = wldc[i].split(".")[3]
+    var stwo = wldc[i].split(".")[2]
+    var sone = wldc[i].split(".")[1]
+    var sonep = JSON.parse(sone)
+    if ( JSON.parse(sthree) <= JSON.parse(sthree) + 3600) { 
     API.chatLog("Moving @" + uj.username + " to position " + wldc[i].split(".")[2])
-    API.moderateAddDJ(uj.id); API.moderateMoveDJ(uj.id, wldc[i].split(".")[2])
+    API.moderateAddDJ(sonep); API.moderateMoveDJ(uj.id, JSON.parse(stwo))
     wldc.splice(i,1); }
     else {
     API.chatLog("@" + uj.username + " took to long to return to the room! Spots are only saved for 30 minutes!")
