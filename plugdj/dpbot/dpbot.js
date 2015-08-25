bc6a89959665f120141ee13cad48fa39280bb733
dcvslab <dcvslab@users.noreply.github.com>
2015-08-24 21:11:48 -0400

    Update dpbot.js

--- plugdj/dpbot/dpbot.js
+++ plugdj/dpbot/dpbot.js
@@ -1,2 +1,7 @@
 //DCV's plug.dj Bot //Simple and easily customizable to make it perfect for your room//
-var version = 1
+function startBot() {
+var dpb = {
+  "settings": JSON.parse(localStorage.getItem("dpsOpt")),
+  "version": "1",
+}
+}
