a8b40be0276423191e42c877e7d127b320a210e0
dcvslab <dcvslab@users.noreply.github.com>
2015-08-25 19:28:57 -0400

    A7.4.3.8B
    It's always nice to fix bugs.

--- plugdj/dps/dpsbeta.js
+++ plugdj/dps/dpsbeta.js
@@ -1,7 +1,7 @@
 //DCV'S PLUGDJ SCRIPT//ALPHA 07 BETA// HTTP://PLUG.DJ/DCV // HTTP://DCVSLAB.GITHUB.IO // //dps@dps.x10host.com DCVSLAB.GITHUB.IO //hi
 if (! on) {
 var on = "on"
-var release = "ALPHA"; var vnum = "7"; var subvnum = "4"; var commitnum = "3.7"; var beta = "BETA"
+var release = "ALPHA"; var vnum = "7"; var subvnum = "4"; var commitnum = "3.8"; var beta = "BETA"
 var version = release + " " + vnum + "." + subvnum + "." + commitnum + " " + beta;
 var sversion = release + "-" + vnum + "-" + subvnum + "-" + commitnum;
 var user = API.getUser();
@@ -518,17 +518,18 @@ API.on(API.ADVANCE, function(data) {
   dpsHa();
 })
 //NOT TOGGLEABLE//
-idspan = document.createElement("span"); idspan.className = "joined"; idspan.style.top = "94px"; //WIP//
+idspan = document.createElement("span"); idspan.className = "joined"; idspan.style.top = "94px"; 
 function dpsIdrollover() {
 if (! document.getElementById("user-rollover")) { } else {
 if (document.getElementById("user-rollover").style.display == "block") {
-  document.getElementsByClassName("meta online")[0].style.height = "125px"
+  document.getElementsByClassName("meta")[14].style.height = "125px"
   var username = document.getElementById("user-rollover").children[0].children[1].innerHTML
   var usersl = API.getUsers().length;
   for (var i = 0; i < usersl; i++) {
     if (API.getUsers()[i].username == username) {
 	idspan.innerHTML = "ID: " + API.getUsers()[i].id; document.getElementById("user-rollover").children[0].appendChild(idspan);     	
 }}}}}
+ setTimeout(function(){ dpsIdrollover; }, 1);
 //CHAT COMMANDS//
 function chatcmd(cmd) {
 if (cmd == "/commands" || cmd == "/cmds") {
