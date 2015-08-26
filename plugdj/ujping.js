026181e2776be7c91418539a42d9252d2bf32296
dcvslab <dcvslab@users.noreply.github.com>
2015-08-26 13:48:08 -0400

    Update ujping.js

--- plugdj/ujping.js
+++ plugdj/ujping.js
@@ -1,1 +1,8 @@
-function userJoin() {
+var badoop = new Audio(); badoop.src = "https://cdn.plug.dj/_/static/sfx/badoop.801a12ca13864e90203193b2c83c019c03a447d1.mp3"; badoop.load();
+function userJoin(uj) {
+  d = new Date()
+  API.chatLog(uj.username + " has joined at " d.getHours() + ":" + d.getMinutes + ":" d.getSeconds)  
+  badoop.play()
+}
+API.on(API.USER_JOIN, userJoin)
+
