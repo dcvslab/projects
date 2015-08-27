1425f2611ef95b38f2d8155020923762f3b09688
dcvslab <dcvslab@users.noreply.github.com>
2015-08-26 22:34:41 -0400

    A7.4.4.3B

--- plugdj/dps/dpsbeta.js
+++ plugdj/dps/dpsbeta.js
@@ -1,7 +1,7 @@
 //DCV'S PLUGDJ SCRIPT//ALPHA 07 BETA// HTTP://PLUG.DJ/DCV // HTTP://DCVSLAB.GITHUB.IO // //dps@dps.x10host.com DCVSLAB.GITHUB.IO //hi
 if (! on) {
 var on = "on"
-var release = "ALPHA"; var vnum = "7"; var subvnum = "4"; var commitnum = "4.2"; var beta = "BETA"
+var release = "ALPHA"; var vnum = "7"; var subvnum = "4"; var commitnum = "4.3"; var beta = "BETA"
 var version = release + " " + vnum + "." + subvnum + "." + commitnum + " " + beta;
 var sversion = release + "-" + vnum + "-" + subvnum + "-" + commitnum;
 var user = API.getUser();
@@ -528,6 +528,8 @@ if (document.getElementById("user-rollover").style.display == "block") {
   for (var i = 0; i < usersl; i++) {
     if (API.getUsers()[i].username == username) {
 	idspan.innerHTML = "ID: " + API.getUsers()[i].id; document.getElementById("user-rollover").children[0].appendChild(idspan);     	
+    } else {
+    	idspan.innerHTML = "ID: UNDEFINED"; document.getElementById("user-rollover").children[0].appendChild(idspan);  
 }}}}}
 setInterval(function(){ dpsIdrollover(); }, 1);
 //CHAT COMMANDS//
