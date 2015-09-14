34a79319be35841bf780fbf8dd7f22ee92caa5e6
dcvslab <dcvslab@users.noreply.github.com>
2015-09-14 19:50:25 -0400

    Update randomtag.js

--- plugdj/randomtag.js
+++ plugdj/randomtag.js
@@ -4,12 +4,13 @@ var randommsgs = [ "%%USER%% do you want to donate and get nothing in reward exc
 "%%USER%% , this is an ad, if you don't want anymore go and donate: http://plug.dj/donate | http://plug.dj/subscribe",
 "%%USER%% , go donate! http://plug.dj/donate | http://plug.dj/subscribe"
 ]
-function randomTag() { 
+function randomTag(user) { 
   var usersl = API.getUsers().length - 1 
   var randomnum = Math.random() * (usersl - 0) + 0; var randommsgnum = Math.random() * (5 - 1) + 1;
   var randomnums = randomnum.toString().split(".")[0]; randommsgsnums = randommsgnum.toString().split(".")[0]
-  var user = API.getUsers()[randomnums].username;
-  var msg = randommsgs[randommsgsnums].replace("%%USER%%", "@" + user)
+  if (user == "RANDOM") { var userc = "@" + API.getUsers()[randomnums].username; } else { var userc = user } 
+  var msg = randommsgs[randommsgsnums].replace("%%USER%%", userc)
   API.sendChat(msg + "")
 }
+function onCommand(cmd) { if (cmd == "/send") { randomTag() }}; API.on(API.CHAT_COMMAND, onCommand);
   
