9b46337f4dbdf8d678d2868baeb61da75f900385
dcvslab <dcvslab@users.noreply.github.com>
2016-11-29 19:54:32 -0500

    Update AdidasATC.tamper.js

--- AdidasATC.tamper.js
+++ AdidasATC.tamper.js
@@ -11,16 +11,17 @@
 
 function start() {
     'use strict';
-    if (document.getElementsByClassName("price-holder")[0]) { location.reload() } else {
+    if (1==10)[0]) { location.reload() } else { //will change to coming soon class
     var a = document.getElementsByClassName("size-dropdown-block")[0].children[0].children[0].children[1].children[1].children[0].children[0].children;
     var b = a.length;
     for (var i = 0; i < b; i++) {
         if (a[i].children[0].innerHTML.includes(".5")) { a[i].children[0].innerHTML = "HALFSIZE" }
     }
-        if (a[i].children[0].innerHTML.includes("11")) { a[i].click() }
+    for (var i = 0; i < b; i++) {
+      if (a[i].children[0].innerHTML.includes("11")) { a[i].click() }}
     document.getElementsByClassName("add-to-cart")[0].click()
     var c = document.getElementsByClassName("size-dropdown-block")[0].children[0].children[0].children[0].children[0].innerHTML
-    if (c == "Select size") { alert("Sold Out") } else {
-        window.location.replace("https://www.adidas.com/us/delivery-start")};
+    if (c == "Select size") { alert("Sold Out"); } else {
+        window.location.replace("https://www.adidas.com/us/delivery-start");}
 }}
 window.onload = start();
