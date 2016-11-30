59f6f158e46ef5a63e7f8449c8a8e81fe16f98ee
dcvslab <dcvslab@users.noreply.github.com>
2016-11-30 16:46:31 -0500

    v3

--- AdidasATC.tamper.js
+++ AdidasATC.tamper.js
@@ -3,39 +3,51 @@
 // @name         Adidas ATC
 // @namespace    none
 // @include      *://adidas.com/*
-// @version      2.3
+// @version      3.0
 // @description  self explaining
 // @author       dcvslab
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
 function start() {
-    if (1==10) { location.reload() } else { //will change to coming soon class
-    var a = document.getElementsByClassName("size-dropdown-block")[0].children[0].children[0].children[1].children[1].children[0].children[0].children;
-    var b = a.length;
-    for (var i = 0; i < b; i++) {
-        if (a[i].children[0].innerHTML.includes(".5")) { var d = a[i].children[0].innerHTML.replace(".", "0"); a[i].children[0].innerHTML = d  }
+  if (document.getElementsByClassName("comingsoon-callout-container")[0]) { location.reload() } else { //will change to coming soon class
+    var da = document.getElementsByClassName("size-dropdown-block")[0].children[0].children[0].children[1].children[1].children[0].children[0].children;
+    var db = da.length;
+    var de = document.getElementsByClassName("add-to-cart")[0]
+    function dS() {
+        var dc = document.getElementsByClassName("size-dropdown-block")[0].children[0].children[0].children[0].children[0].innerHTML
+        if (dc == "Select size") { alert("Sold Out"); } else {
+            window.location.replace("https://www.adidas.com/us/delivery-start");}
     }
-    for (var i = 0; i < b; i++) {
-      if (a[i].children[0].innerHTML.includes("11")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("10")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("9")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("8")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("12")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("1005")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("905")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("805")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("805")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("1105")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("7")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("6")) { a[i].click() }
-      else { if (a[i].children[0].innerHTML.includes("5")) { a[i].click() }
-     }}}}}}}}}}}}}
-    document.getElementsByClassName("add-to-cart")[0].click()
-    var c = document.getElementsByClassName("size-dropdown-block")[0].children[0].children[0].children[0].children[0].innerHTML
-    if (c == "Select size") { alert("Sold Out"); } else {
-        window.location.replace("https://www.adidas.com/us/delivery-start");}
-}};
+    for (var i = 0; i < db; i++) { //FIND SIZES, ORDERED BEST TO WORST
+        var df = da[i].children[0].innerHTML
+        if (df.includes("11")) { if (df.includes(".")) {} else { da[i].click(); de.click(); dS(); }} else { 
+            if (df.includes("10")) { if (df.includes(".")) {} else { da[i].click(); de.click(); dS(); }} else {
+                if (df.includes("9")) { if (df.includes(".")) {} else { da[i].click(); de.click(); dS(); }} else {
+                    if (df.includes("8")) { if (df.includes(".")) {} else { da[i].click(); de.click(); dS(); }} else {
+                        if (df.includes("12")) { if (df.includes(".")) {} else { da[i].click(); de.click(); dS(); }} else {
+                            if (df.includes("10.5")) { da[i].click(); de.click(); dS(); } else {
+                                if (df.includes("9.5")) { da[i].click(); de.click(); dS(); } else {
+                                    if (df.includes("8.5")) { da[i].click(); de.click(); dS(); } else {
+                                        if (df.includes("11.5")) { da[i].click(); de.click(); dS(); } else {
+                                            if (df.includes("7")) { if (df.includes(".")) {} else { da[i].click(); de.click(); dS(); }} else { 
+                                                if (df.includes("6")) { if (df.includes(".")) {} else { da[i].click(); de.click(); dS(); }} else {
+                                                    if (df.includes("5")) { if (df.includes(".")) {} else { da[i].click(); de.click(); dS(); }} else {
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } 
+    } //for loop end
+   }; //end if function
+}; //end start()
 window.onload = function() {
     start();
 };
