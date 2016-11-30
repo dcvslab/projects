d3e2b9ed721e16673652a91726bd7b988936ec96
dcvslab <dcvslab@users.noreply.github.com>
2016-11-29 20:40:31 -0500

    Update AdidasATC.tamper.js

--- AdidasATC.tamper.js
+++ AdidasATC.tamper.js
@@ -3,7 +3,7 @@
 // @name         Adidas ATC
 // @namespace    none
 // @include      *://adidas.com/*
-// @version      2
+// @version      1.2
 // @description  self explaining
 // @author       dcvslab
 // @match        *://*/*
@@ -14,10 +14,23 @@ function start() {
     var a = document.getElementsByClassName("size-dropdown-block")[0].children[0].children[0].children[1].children[1].children[0].children[0].children;
     var b = a.length;
     for (var i = 0; i < b; i++) {
-        if (a[i].children[0].innerHTML.includes(".5")) { a[i].children[0].innerHTML = "HALFSIZE" }
+        if (a[i].children[0].innerHTML.includes(".5")) { a[i].children[0].replace(".", "0") }
     }
     for (var i = 0; i < b; i++) {
-      if (a[i].children[0].innerHTML.includes("11")) { a[i].click() }}
+      if (a[i].children[0].innerHTML.includes("11")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("10")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("9")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("8")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("12")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("1005")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("905")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("805")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("805")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("1105")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("7")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("6")) { a[i].click() }
+      else { if (a[i].children[0].innerHTML.includes("5")) { a[i].click() }
+    }
     document.getElementsByClassName("add-to-cart")[0].click()
     var c = document.getElementsByClassName("size-dropdown-block")[0].children[0].children[0].children[0].children[0].innerHTML
     if (c == "Select size") { alert("Sold Out"); } else {
