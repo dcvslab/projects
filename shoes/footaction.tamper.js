38dcd814f40d0e510302e8c71b22cecd07ad8ae2
dcvslab <dcvslab@users.noreply.github.com>
2016-12-02 22:13:06 -0500

    Create footaction.tamper.js

---
+++ shoes/footaction.tamper.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         FootAction ATC
+// @namespace    none
+// @include      *://footaction.com/*
+// @version      1.0
+// @description  self explaining
+// @author       dcvslab
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
