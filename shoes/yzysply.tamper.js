9e5f465b5fbc9b8a9ba128797bf411a5121a1f87
Dean <dcvslab@users.noreply.github.com>
2017-12-16 10:01:41 -0500

    Update yzysply.tamper.js

--- shoes/yzysply.tamper.js
+++ shoes/yzysply.tamper.js
@@ -1,7 +1,17 @@
+// ==UserScript==
+// @name         YZY SPLY
+// @namespace    none
+// @include      *://yeezysupply.com/*
+// @version      1
+// @description  autorefresh
+// @author       dcvslab
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
 function yzySply() {
   var x = document.getElementsByClassName("PGI__bg js-img-1")[0]
   if (! x) { alert("NULL") }
   else { setTimeout(function(){ location.reload(); }, 5000); }
   }
 }
-yzySply()
+window.onload = function() {yzySply()}
