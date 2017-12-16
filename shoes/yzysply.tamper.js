56c691fd5271a29fc635f1e46ca6160563a29b1a
Dean <dcvslab@users.noreply.github.com>
2017-12-16 09:56:15 -0500

    Create yzysply.tamper.js

---
+++ shoes/yzysply.tamper.js
@@ -0,0 +1,7 @@
+function yzySply() {
+  var x = document.getElementsByClassName("PGI__bg js-img-1")[0]
+  if (! x) { alert("NULL") }
+  else { setTimeout(function(){ location.reload(); }, 5000); }
+  }
+}
+yzySply()
